1584f159a0b0fad57af9b4516581dead1c8a5827
Bongo9911 <dawsonbuist@gmail.com>
2023-09-17 13:11:16 -0400

    fixed graph command

--- commands/info/graph.js
+++ commands/info/graph.js
@@ -38,98 +38,101 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply();
 
-        interaction.followUp({content: "This command is currently disabled"});
-        // const activeGame = await Games.findOne({
-        //     where: {
-        //         guild_id: interaction.guildId,
-        //         channel_id: interaction.channelId,
-        //         active: true
-        //     }
-        // });
+        // interaction.followUp({ content: "This command is currently disabled" });
+        const activeGame = await Games.findOne({
+            where: {
+                guild_id: interaction.guildId,
+                channel_id: interaction.channelId,
+                active: true
+            }
+        });
 
-        // if (activeGame) {
-        //     const items = await GameItems.findAll({
-        //         where: {
-        //             game_id: activeGame.id
-        //         }
-        //     })
+        if (activeGame) {
+            const items = await GameItems.findAll({
+                where: {
+                    game_id: activeGame.id
+                }
+            })
 
-        //     let fullData = [];
+            let fullData = [];
 
-        //     let skipNum = Math.max(Math.round((activeGame.turns + 1) / 200), 1);
+            let skipNum = Math.max(Math.round((activeGame.turns + 1) / 200), 1);
 
-        //     for (let i = 0; i < items.length; ++i) {
-        //         const history = await GameHistory.findAll({
-        //             where: {
-        //                 game_id: activeGame.id,
-        //                 item_id: items[i].id
-        //             },
-        //             order: [['turn_number', 'ASC']]
-        //         });
+            for (let i = 0; i < items.length; ++i) {
+                const history = await GameHistory.findAll({
+                    where: {
+                        game_id: activeGame.id,
+                        item_id: items[i].id
+                    },
+                    order: [['turn_number', 'ASC']]
+                });
 
-        //         fullData[i] = [];
+                fullData[i] = [];
 
-        //         for (let h = 0; h < history.length; h += skipNum) {
-        //             while (fullData[i].length !== Math.floor(history[h].turn_number / skipNum)) {
-        //                 fullData[i].push(history[h - 1].points);
-        //             }
-        //             fullData[i].push(history[h].points);
-        //         }
+                let historyIndex = 0;
+                for (let h = 0; h <= activeGame.turns; h += skipNum) {
+                    while (history[historyIndex].turn_number < h && historyIndex !== history.length - 1) {
+                        historyIndex++;
+                    }
 
-        //         while (fullData[i].length !== Math.floor((activeGame.turns + 1) / skipNum)) {
-        //             fullData[i].push(history[history.length - 1].points);
-        //         }
-        //     }
+                    if (history[historyIndex].turn_number === h || historyIndex === 0) {
+                        fullData[i].push(history[historyIndex].points);
+                    }
+                    else {
+                        fullData[i].push(history[historyIndex - 1].points);
+                    }
+                }
+            }
 
-        //     let datasets = [];
+            let datasets = [];
 
-        //     for (let i = 0; i < items.length; ++i) {
-        //         datasets.push({
-        //             label: items[i].name,
-        //             data: fullData[i],
-        //             fill: false,
-        //             //TODO: Color is in hex right now, need to convert to RGB
-        //             borderColor: items[i].color ? ['rgba(' + items[i].color[0] + "," + items[i].color[1] + "," + items[i].color[2] + ", 1)"] :
-        //                 ['rgba(' + Math.round(colors[i % colors.length][0] / (1 + Math.floor(i / colors.length))) + ',' + Math.round(colors[i % colors.length][1] / (1 + Math.floor(i / colors.length)))
-        //                     + ',' + Math.round(colors[i % colors.length][2] / (1 + Math.floor(i / colors.length))) + ', 1)'],
-        //             backgroundColor: items[i].color ? 'rgba(' + items[i].color[0] + "," + items[i].color[1] + "," + items[i].color[2] + ", 1)" :
-        //                 'rgba(' + Math.round(colors[i % colors.length][0] / (1 + Math.floor(i / colors.length))) + ',' + Math.round(colors[i % colors.length][1] / (1 + Math.floor(i / colors.length)))
-        //                 + ',' + Math.round(colors[i % colors.length][2] / (1 + Math.floor(i / colors.length))) + ', 1)' //0.2
-        //         })
-        //     }
+            for (let i = 0; i < items.length; ++i) {
+                datasets.push({
+                    label: items[i].name,
+                    data: fullData[i],
+                    fill: false,
+                    //TODO: Color is in hex right now, need to convert to RGB
+                    borderColor: items[i].color ? ['rgba(' + items[i].color[0] + "," + items[i].color[1] + "," + items[i].color[2] + ", 1)"] :
+                        ['rgba(' + Math.round(colors[i % colors.length][0] / (1 + Math.floor(i / colors.length))) + ',' + Math.round(colors[i % colors.length][1] / (1 + Math.floor(i / colors.length)))
+                            + ',' + Math.round(colors[i % colors.length][2] / (1 + Math.floor(i / colors.length))) + ', 1)'],
+                    backgroundColor: items[i].color ? 'rgba(' + items[i].color[0] + "," + items[i].color[1] + "," + items[i].color[2] + ", 1)" :
+                        'rgba(' + Math.round(colors[i % colors.length][0] / (1 + Math.floor(i / colors.length))) + ',' + Math.round(colors[i % colors.length][1] / (1 + Math.floor(i / colors.length)))
+                        + ',' + Math.round(colors[i % colors.length][2] / (1 + Math.floor(i / colors.length))) + ', 1)' //0.2
+                })
+            }
 
-        //     const chart = new QuickChart();
-        //     chart.setConfig({
-        //         type: "line",
-        //         data: {
-        //             labels: [...Array(fullData[0].length).fill().map((_, idx) => idx * skipNum)],
-        //             datasets: datasets
-        //         },
-        //         options: {
-        //             elements: {
-        //                 point: {
-        //                     radius: 0
-        //                 }
-        //             }
-        //         }
-        //     })
-        //         .setWidth(800)
-        //         .setHeight(600)
-        //         .setVersion("4.3.2");
+            const chart = new QuickChart();
+            chart.setConfig({
+                type: "line",
+                data: {
+                    labels: [...Array(fullData[0].length).fill().map((_, idx) => idx * skipNum)],
+                    datasets: datasets
+                },
+                options: {
+                    elements: {
+                        point: {
+                            radius: 0
+                        }
+                    }
+                }
+            })
+                .setWidth(800)
+                .setHeight(600)
+                .setVersion("4.3.2");
 
-        //     const chartURL = await chart.getShortUrl();
+            const chartURL = await chart.getShortUrl();
 
-        //     const graphEmbed = new EmbedBuilder()
-        //         .setColor('#0099ff')
-        //         .setTitle("Point Graph")
-        //         .setImage(chartURL)
-        //         .setTimestamp()
-        //         .setFooter({ text: '/graph', iconURL: 'https://i.imgur.com/kk9lhk3.png' });
+            const graphEmbed = new EmbedBuilder()
+                .setColor('#0099ff')
+                .setTitle("Point Graph")
+                .setImage(chartURL)
+                .setTimestamp()
+                .setFooter({ text: '/graph', iconURL: 'https://i.imgur.com/kk9lhk3.png' });
 
-        //     interaction.followUp({ embeds: [graphEmbed] });
-        // }
-        // else {
-        //     interaction.followUp({ content: "There is currently no active game in this channel." });
-        // }
+            interaction.followUp({ embeds: [graphEmbed] });
+        }
+        else {
+            interaction.followUp({ content: "There is currently no active game in this channel." });
+        }
     },
 };
