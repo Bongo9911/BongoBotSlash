4e253face6a4392553d2bff311e5e2deb61b0294
Bongo9911 <dawsonbuist@gmail.com>
2024-05-12 16:47:53 -0400

    fix ordinals for rankings

--- commands/info/badgeboard.js
+++ commands/info/badgeboard.js
@@ -52,17 +52,17 @@ module.exports = {
 };
 
 
-function getRankString(n) {
-    if (n == 1) {
-        return "1st";
+function getRankString(i) {
+    let j = i % 10,
+        k = i % 100;
+    if (j === 1 && k !== 11) {
+        return i + "st";
     }
-    else if (n == 2) {
-        return "2nd";
+    if (j === 2 && k !== 12) {
+        return i + "nd";
     }
-    else if (n == 3) {
-        return "3rd";
+    if (j === 3 && k !== 13) {
+        return i + "rd";
     }
-    else {
-        return n + "th";
-    }
+    return i + "th";
 }
